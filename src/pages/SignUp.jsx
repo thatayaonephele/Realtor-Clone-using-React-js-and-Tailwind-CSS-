f5ab99e649f5f570a946c6c4cf8b7de19ad5214b
thatiphele <thatayaonephele@gmail.com>
2023-12-08 13:02:38 +0200

    install firebase and react-toastify and sign up the user

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,18 +2,18 @@ import React, { useState } from 'react'
 import {AiFillEye, AiFillEyeInvisible} from "react-icons/ai"
 import { Link } from 'react-router-dom';
 import OAuth from '../components/OAuth';
-import 
-{getAuth, 
-  createUserWithEmailAndPassword, 
+import
+{getAuth,
+  createUserWithEmailAndPassword,
   updateProfile} from "firebase/auth";
-import {db} from "../firebase";
-import {doc, serverTimestamp, setDoc} from 
+import {db} from "../firebase"; //The database we created inside the firestore
+import {doc, serverTimestamp, setDoc} from
 "firebase/firestore";
-import {useNavigate} from "react-router-dom"; 
+import {useNavigate} from "react-router-dom"; //To allow user to navigate to the home page
 import {toast} from "react-toastify";
 export default function SignUp() {
-  const [showPassword, setShowPassword] = 
-  useState(false); 
+  const [showPassword, setShowPassword] =
+  useState(false);
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -33,8 +33,8 @@ export default function SignUp() {
     try {
       const auth = getAuth();
       const userCredential = await createUserWithEmailAndPassword(
-        auth, 
-        email, 
+        auth,
+        email,
         password
         );
         updateProfile(auth.currentUser, {
@@ -42,79 +42,79 @@ export default function SignUp() {
         })
       const user = userCredential.user;
       const formDataCopy = {...formData}
-      delete formData.password //delete the password save as its more secure to use Firebase password storage
-      formDataCopy.timestamp = serverTimestamp
+      delete formData.password //delete the password save as its more secure to use Firebase password storage because you dont want to save the password on the database
+      formDataCopy.timestamp = serverTimestamp //To show the time the user is registered,using a method that captures the time the server is accessed
       ();
 
-      await setDoc(doc(db, "users", user.uid), formDataCopy); //"users" is what I'll see under Cloud FireStore "start collection"
+      await setDoc(doc(db, "users", user.uid), formDataCopy); //"users" is what I'll see under Cloud FireStore "start collection. user uid on our database is accessed using user.uid"
       //toast.success("Sign Up was successful!");
       navigate("/"); //Navigate to home page after adding the china onto db
     } catch (error) {
       toast.error("Something Went Wrong with the registration");
-    }
+    } //I should use an if statement for if both invalid and invalid messaging...but not neccessary if sign up was successful
   }
   return (
     <section>
       <h1 className="text-3xl text-center mt-5 font-bold">Sign Up
       </h1>
-      <div className="flex 
-      justify-center 
-      flex-wrap 
-      items-center 
+      <div className="flex
+      justify-center
+      flex-wrap
+      items-center
       px-6 py-12 max-w-6lx mx-auto" >
         <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
           <img src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8a2V5fGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="key" className="w-full rounded-2xl"/>
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
           <form onSubmit={onSubmit}>
-            <input type="text" 
-            id="name" 
-            value={name} 
-            onChange={onChange} 
-            placeholder="Full Name" 
+            <input type="text"
+            id="name"
+            value={name}
+            onChange={onChange}
+            placeholder="Full Name"
             className=" w-full px-4 py-2 text-xl
-             text-gray-700 bg-white border-gray-300 
+             text-gray-700 bg-white border-gray-300
             rounded transition-ease-out mb-6"
             />
 
-            <input type={"email"} 
-            id="email" value={email} 
-            onChange={onChange} 
-            placeholder="Email Address" 
-            className="w-full 
-            px-4 py-2 
-            text-xl text-gray-700 bg-white border-gray-300 
+            <input type={"email"}
+            id="email" value={email}
+            onChange={onChange}
+            placeholder="Email Address"
+            className="w-full
+            px-4 py-2
+            text-xl text-gray-700 bg-white border-gray-300
             rounded transition-ease-out mb-6"
             />
           <div className="relative mb-6">
-            <input type={showPassword ? "text" : "password"} id="password" value={password} onChange={onChange} placeholder="Password" className="w-full 
-            px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 
-            rounded transition-ease-out" 
+            <input type={showPassword ? "text" : "password"} id="password" value={password} onChange={onChange} placeholder="Password" className="w-full
+            px-4 py-2 text-xl text-gray-700 bg-white border-gray-300
+            rounded transition-ease-out"
             />
           {showPassword ? (
           <AiFillEyeInvisible className="absolute right-3 top-3 text-xl cursor-pointer" onClick={()=>setShowPassword((prevState)=>!prevState)} />) : (<AiFillEye className="absolute right-3 top-3 text-xl cursor-pointer" onClick={()=>setShowPassword((prevState)=>!prevState)}/>)}
           </div>
-          <div className="flex 
-          justify-between 
-          whitespace-nowrap 
+          <div className="flex
+          justify-between
+          whitespace-nowrap
           text-sm sm:text-lg py-3">
             <p>Have an account?
-              <Link to="/sign-up" 
+              <Link to="/sign-up"
               className="text-red-600 hover:text-700
-              transition duration-200 
+              transition duration-200
               ease-in-out ml-1">
                 Sign In
               </Link>
              </p>
              <p>
               <Link to="/forgot-password" className="text-blue-600 hover:text-800
-              transition duration-200 
+              transition duration-200
               ease-in-out">Forgot Password?</Link>
              </p>
           </div>
           <button className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out
           hover:shadow-lg
-          active:bg-blue-800" 
+          active:bg-blue-800"
           type="submit">
             Sign Up
           </button>
@@ -123,8 +123,8 @@ export default function SignUp() {
           before:border-gray-300
           after:border-t after:flex-1
           after:border-gray-300">
-            <p className="text-center 
-            justify-center 
+            <p className="text-center
+            justify-center
             font-semibold mx-4">
               OR
             </p>
