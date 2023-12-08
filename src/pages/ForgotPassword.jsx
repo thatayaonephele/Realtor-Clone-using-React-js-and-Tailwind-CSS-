fb4e46b7f178887096e06719136a6d776dd42206
thatiphele <thatayaonephele@gmail.com>
2023-12-08 17:01:10 +0200

    complete the oauth functionality

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,13 +1,16 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
-import OAuth from '../components/OAuth';
-import { toast } from "react-toastify";
 import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { toast } from "react-toastify";
+import OAuth from '../components/OAuth';
+
 export default function ForgotPassword() {
   const [email, setEmail] = useState("");
   function onChange(e) {
+
     setEmail(e.target.value);
   }
+
     async function onSubmit (e) {
       e.preventDefault();
       try {
@@ -20,48 +23,43 @@ export default function ForgotPassword() {
   }
   return (
     <section>
-      <h1 className="text-3xl text-center mt-5 font-bold">Forgot Password
-      </h1>
-      <div className="flex 
-      justify-center 
-      flex-wrap 
-      items-center 
-      px-6 py-12 max-w-6lx mx-auto" >
+      <h1 className="text-3xl text-center mt-5 font-bold">Forgot Password</h1>
+      <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6lx mx-auto" >
         <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
           <img src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8a2V5fGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="key" className="w-full rounded-2xl"/>
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
           <form onSubmit={onSubmit}>
-            <input 
+            <input
             type="email"
-            id="email" value={email} 
-            onChange={onChange} 
-            placeholder="Email Address" 
-            className="mb-6 w-full 
-            px-4 py-2 
-            text-xl text-gray-700 bg-white border-gray-300 
+            id="email" value={email}
+            onChange={onChange}
+            placeholder="Email Address"
+            className="mb-6 w-full
+            px-4 py-2
+            text-xl text-gray-700 bg-white border-gray-300
             rounded transition-ease-out"
             />
-          <div className="flex 
-          justify-between 
-          whitespace-nowrap 
+          <div className="flex
+          justify-between
+          whitespace-nowrap
           text-sm sm:text-lg py-3">
             <p className="mb-6">Don't have an account?
-              <Link to="/sign-up" 
+              <Link to="/sign-up"
               className="text-red-600 hover:text-700
-              transition duration-200 
+              transition duration-200
               ease-in-out ml-1">Register
               </Link>
              </p>
              <p>
               <Link to="/sign-in" className="text-blue-600 hover:text-800
-              transition duration-200 
+              transition duration-200
               ease-in-out">Sign In Instead?</Link>
              </p>
           </div>
           <button className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out
           hover:shadow-lg
-          active:bg-blue-800" 
+          active:bg-blue-800"
           type="submit">
             Send Reset Password
           </button>
@@ -70,8 +68,8 @@ export default function ForgotPassword() {
           before:border-gray-300
           after:border-t after:flex-1
           after:border-gray-300">
-            <p className="text-center 
-            justify-center 
+            <p className="text-center
+            justify-center
             font-semibold mx-4">
               OR
             </p>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,15 +1,28 @@
 import { getAuth, updateProfile } from 'firebase/auth';
-import { useNavigate } from 'react-router-dom';
-import React, { useState } from 'react';
-import {doc, updateDoc} from 'firebase/firestore';
-import { db } from "../firebase";
-import {toast} from "react-toastify";
-import {FcHome} from "react-icons/fc";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  orderBy,
+  query,
+  updateDoc,
+  where,
+} from 'firebase/firestore';
+  import { useState } from "react";
+  import { Link, useNavigate } from "react-router-dom";
+  import { toast } from "react-toastify";
+  import { db } from "../firebase";
+  import { FcHome } from "react-icons/fc";
+  import { useEffect } from "react";
+  import ListingItem from "../components/ListingItem";
 
-export default function Profile() {
+  export default function Profile() {
   const auth =  getAuth();
   const navigate = useNavigate();
   const [changeDetail, setChangeDetail] = useState(false);
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
@@ -45,7 +58,39 @@ async function onSubmit() {
       toast.error("Could not update the profile details");
     }
   }
-  return
+  useEffect(() => {
+    async function fetchUserListings() {
+      const listingRef = collection(db, "listings");
+      const q = query(
+        listingRef,
+        where("userRef", "==", auth.currentUser.uid),
+        orderBy("timestamp", "desc")
+      );
+      const querySnap = await getDocs(q);
+      let listings = [];
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+      setListings(listings);
+      setLoading(false);
+    }
+    fetchUserListings();
+  }, [auth.currentUser.uid]);
+  async function onDelete(listingID) {
+    if (window.confirm("Are you sure you want to delete?")) {
+      await deleteDoc(doc(db, "listings", listingID));
+      const updatedListings = listings.filter(
+        (listing) => listing.id !== listingID
+      );
+      setListings(updatedListings);
+      toast.success("Successfully deleted the listing");
+    }
+  }
+  function onEdit(listingID) {
+    navigate(`/edit-listing/${listingID}`);
 }
 return (
   <>
@@ -82,23 +127,29 @@ return (
               <span
               onClick={() => {
                 changeDetail && onSubmit();
-                setChangeDetail((prevState) => {!prevState});
+                setChangeDetail((prevState) => !prevState);
               }}
                 className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer"
               >
-              {changeDetail ? "Apply Change": "Edit"}</span>
+              {changeDetail ? "Apply Change": "Edit"}
+              </span>
             </p>
             <p
-              onClick={onLogout} className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer"
+              onClick={onLogout}
+              className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer"
             >
               Sign out
             </p>
           </div>
         </form>
-        <button type="submit" className='w-full bg-blue-600
-        text-white uppercase px-7 py-3 text-sm font-medium
-        rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800'>
-          <Link to="/create-listing" className='flex justify-center items-center'>
+        <button
+        type="submit"
+        className='w-full bg-blue-600 selection: text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800'
+        >
+          <Link
+          to="/create-listing"
+          className='flex justify-center items-center'
+          >
             <FcHome className='mr-2 text-3xl  bg-red-200 rounded-full p-1 border-2'/>
             Sell or rent your home
           </Link>
@@ -106,6 +157,24 @@ return (
       </div>
     </section>
     <div className="max-w-6xl px-3 mt-6 mx-auto">
+    {!loading && listings.length > 0 && (
+          <>
+            <h2 className="text-2xl text-center font-semibold mb-6">
+              My Listings
+            </h2>
+            <ul className="sm:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+              {listings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  id={listing.id}
+                  listing={listing.data}
+                  onDelete={() => onDelete(listing.id)}
+                  onEdit={() => onEdit(listing.id)}
+                />
+              ))}
+            </ul>
+          </>
+        )}
     </div>
   </>
-);
+);}
